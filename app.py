38a769ea012c9ebcaa3e7fe74f94e11f9be298a6
Ranjit Singh <78042062+Ranjit-Singh-786@users.noreply.github.com>
2024-03-30 17:39:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from  flask import Flask , render_template , url_for
+from  flask import Flask , render_template , url_for,request
 
 app = Flask(__name__)
 
@@ -24,7 +24,30 @@ def service():
 @app.route('/course',methods=['GET','POST'])
 def course():
     return "Hii course"
-# route of this: http://127.0.0.1:5000/contact
+# route of this: http://127.0.0.1:5000/course
+
+
+
+
+@app.route('/MLAPI',methods=['GET','POST'])
+def MLAPI():
+    return render_template('testing.html')
+# route of this: http://127.0.0.1:5000/MLAPI
+
+
+
+@app.route('/testing',methods=['GET','POST'])
+def testing():
+    if request.method == 'POST':
+        option = request.form['options']
+        user_name = request.form['user_name']
+        password = request.form['password']
+        secret_code = request.form['secret_code']
+        user_detail = {'user_name':user_name,'password':password,'secre_code':secret_code,'orgainsation':option}
+
+        details_lst = [user_name,password,secret_code,option]
+        return render_template('testing.html',user_details=user_detail,detail_list=details_lst)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
