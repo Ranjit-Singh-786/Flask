57a05952493cb00f7b6632d91f0fba8849ebab9e
Ranjit Singh <78042062+Ranjit-Singh-786@users.noreply.github.com>
2024-03-30 17:29:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request,render_template,url_for
+from  flask import Flask , render_template , url_for
 
 app = Flask(__name__)
 
@@ -6,16 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/testing',methods=['GET','POST'])
-def testing():
-    if request.method == 'POST':
-        option = request.form['options']
-        user_name = request.form['user_name']
-        password = request.form['password']
-        secret_code = request.form['secret_code']
-        user_detail = {'user_name':user_name,'password':password,'secre_code':secret_code,'orgainsation':option}
+@app.route('/about',methods=['GET','POST'])
+def about():
+    return "Hii about"
+# route of this: http://127.0.0.1:5000/about
 
-        details_lst = [user_name,password,secret_code,option]
-        return render_template('index.html',user_details=user_detail,detail_list=details_lst)
-if __name__=="__main__":
+@app.route('/contact',methods=['GET','POST'])
+def contact():
+    return "Hii contact"
+# route of this: http://127.0.0.1:5000/contact
+
+@app.route('/service',methods=['GET','POST'])
+def service():
+    return "Hii service"
+# route of this: http://127.0.0.1:5000/service
+
+@app.route('/course',methods=['GET','POST'])
+def course():
+    return "Hii course"
+# route of this: http://127.0.0.1:5000/contact
+
+if __name__ == "__main__":
     app.run(debug=True)
